4b493e74bf70a3f7c4827f91797d765b47295290
Alec <alec.glavey@gmail.com>
2019-09-07 19:27:32 -0700

    Update penniesForPay.py

--- penniesForPay.py
+++ penniesForPay.py
@@ -23,3 +23,14 @@ else:
 
     # Print total salary and exit program
     print('Total salary by day ', day,  ' is $', format(totalSalary, ',.2f'), sep='' )
+    
+    
+"   
+Pennies for Pay
+
+Write a program that calculates the amount of money a person would earn over a period of time if his or her salary is one penny the first day, two pennies the second day, and continues to double each day. The program should ask the user for the number of days.
+
+Display a table showing what the salary was for each day, and then show the total pay at the end of the period. The output should be displayed in a dollar amount, not the number of pennies.
+
+Input Validation: Don’t accept less than equal zero or more than 64 (accept 1-64).
+"
